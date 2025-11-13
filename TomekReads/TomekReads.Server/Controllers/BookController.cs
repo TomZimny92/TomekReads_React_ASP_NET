d2eb31a34b28f7b0131ca7345d706bd119b91c44
Tom Zimny <64618507+TomZimny92@users.noreply.github.com>
2025-11-12 20:15:23 -0500

    for real this time

---
+++ TomekReads/TomekReads.Server/Controllers/BookController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TomekReads.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController : ControllerBase
+    {
+        private readonly IBookService _bookService;
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IEnumerable<Book>>> GetAllBooksAsync()
+        {
+            try
+            {
+                var books = await _bookService.GetAllAsync();
+                if (books == null)
+                {
+                    return NotFound();
+                }
+                return Ok(books);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Book>> GetBookAsync(int id)
+        {
+            try
+            {
+                var book = await _bookService.GetBookAsync(id);
+                if (book == null)
+                {
+                    return NotFound();
+                }
+                return Ok(book);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<Book>> AddBookAsync(Book book)
+        {
+            try
+            {
+
+            }
+            await _bookService.AddBookAsync(book);
+            var newBook = await _bookService.GetBookAsync(book.id);
+            if (newBook == null)
+            {
+                return
+            }
+        }
+    }
+}
---
+++ TomekReads/tomekreads.client/src/App.tsx
@@ -0,0 +1,34 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+          <h3>Books!</h3>
+          {books.map((book) => {
+              <BookView book={book} />
+          }) }
+    </>
+  )
+}
+
+// onMount, get all existing books via api and put it in a list
+
+// the add button will use a modal and api to create a new book
+    // when typing a book in the modal, use 3rd party api to help with the search and autofill the relevant fields with retrieved data
+
+// when a book is added, the new entry is added to the books list
+// i don't want to run api if i don't have to
+
+// the page is re-rendered with the new book at the top
+
+// search bar to filter books list
+function LoadBooks(): void {
+    
+}
+
+export default App
