7db2251cf762bbbc50e469e0d3e3838dd285816c
Tom Zimny <64618507+TomZimny92@users.noreply.github.com>
2025-11-20 08:01:42 -0500

    unit testing setup

---
+++ TomekReads/TomekReads.Server.Test/BookServiceShould.cs
@@ -0,0 +1,27 @@
+﻿using TomekReads.Server.Data.Services;
+
+namespace TomekReads.Server.Test
+{
+    public class BookServiceShould
+    {
+        private BookService _bookService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _bookService = new BookService();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Fail();
+        }
+    }
+}
